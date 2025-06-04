9837c42418e91b0b6a581739fa189b6d0f2fcdfc
legrs <legrs.kinoko@gmail.com>
2025-06-04 18:13:06 +0900

    add csvtest

--- main.js
+++ main.js
@@ -4,22 +4,22 @@ let cimg = document.getElementById("cImg");
     // consts
 const ctx = canv.getContext("2d");
 const yOfs = 120;
-const stars = [
-[0,0],
-[20,0]
-];
     // varialbes
 let height = 1000;
 let width = 1000;
 
-let turb; // 大気ゆらぎ
-let back; // 背景の明るさ
-let diam; // 口径
-let magn; // 倍率
-let fl = 130; //現実で使用するレンズの倍率
+let turb; // 大気ゆらぎ mdeg
+let back; // 背景の明るさ mag
+let diam; // 口径 mm
+let magn; // 倍率 倍
+//let fl = 130; //現実で使用するレンズの倍率
 
+let request = new XMLHttpRequest();
+request.open("GET", "./stars/m81_m82.txt", false);
+request.send();
+const starsData = request.responseText;
 
-
+console.log(starsData);
 
 
 function resizeWindow(){
@@ -27,8 +27,8 @@ function resizeWindow(){
     height = window.innerHeight;
     canv.width = width;
     canv.height = height;
-    cimg.style.width = Math.min(width,height);    
-    cimg.style.height = Math.min(width,height);    
+    //cimg.style.width = Math.min(width,height);    
+    //cimg.style.height = Math.min(width,height);    
 }
 window.onresize = resizeWindow;
 resizeWindow();
